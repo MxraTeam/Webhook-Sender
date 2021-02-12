b90f80c7c2f8d60d41660363cafb95801b41b259
FurkanPM <77110754+FurkanPM@users.noreply.github.com>
2021-02-12 22:28:00 +0300

    Add files via upload

---
+++ Webhook/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webhook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendwebhook(textBox1.Text, textBox3.Text, textBox2.Text);
+            textBox3.Text = "";
+        }
+        public static void sendwebhook(string bağlantı, string mesaj, string ad)
+        {
+            Http.Post(bağlantı, new System.Collections.Specialized.NameValueCollection()
+            {
+            {
+                "ad",
+                ad
+            },
+            {
+                "content",
+                mesaj
+            }
+            });
+        }
+    }
+}
